759b27356cc9a8885d6f1c239ef2e3821d8a6e0d
Abdurohman <su1600439@su.edu.et>
2026-08-20 22:43:21 +0300

    Completed Day 107: Added custom Is OwnerOrReadOnly permissions, search, filtering, and pagination

--- books/views.py
+++ books/views.py
@@ -3,6 +3,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from django.contrib.auth.models import User
 from .models import Book
 from .serializers import RegisterSerializer, BookSerializer
+from .permissions import IsOwnerOrReadOnly
+from .pagination import BookPagination
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -13,6 +15,7 @@ class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all().order_by('-created_at')
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = BookPagination
     
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['category', 'author']
@@ -25,7 +28,7 @@ class BookListCreateView(generics.ListCreateAPIView):
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_update(self, serializer):
         serializer.save(owner=self.request.user)
