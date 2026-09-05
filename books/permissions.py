37dffeb24afeea19df581c7b889392e7d1ae465e
Abdurohman <su1600439@su.edu.et>
2026-09-05 07:02:21 +0300

    Add IsOwnerOrStaffCanEditDelete custom permission

--- books/permissions.py
+++ books/permissions.py
@@ -1,7 +1,13 @@
 from rest_framework import permissions
 
-class IsStaffOrReadOnly(permissions.BasePermission):
+class IsOwnerOrStaffCanEditDelete(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return bool (request.user and request.user.is_authenticated and (request.user.is_staff or request.user.is_superuser))
+        return bool(request.user and request.user.is_authenticated)
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool( obj.owner == request.user 
+                    or request.user.is_staff 
+                    or request.user.is_superuser)
